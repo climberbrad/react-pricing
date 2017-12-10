5bccd569ecb0ef5dbd058a30509f55ce234e517d
brad.jordan <brad.jordan@cloudability.com>
2017-12-10 16:12:08 -0700

    added more data columns

--- src/components/price_item.js
+++ src/components/price_item.js
@@ -3,13 +3,17 @@ import React from "react";
 export default (props) => {
     return (
         <tr>
-            <td>{props.instanceType}</td>
-            <td>{props.region}</td>
-            <td>{props.termType}</td>
-            <td>{props.leaseContractLength}</td>
-            <td>{props.purchaseOption}</td>
-            <td>{props.offeringClass}</td>
-            <td>${props.pricePerUnit}</td>
+            <td className="table-data">{props.instanceType}</td>
+            <td className="table-data">{props.region}</td>
+            <td className="table-data">{props.termType}</td>
+            <td className="table-data">{props.leaseContractLength}</td>
+            <td className="table-data">{props.purchaseOption}</td>
+            <td className="table-data">{props.offeringClass}</td>
+            <td className="table-data">${props.pricePerUnit}</td>
+            <td className="table-data">{props.memory}</td>
+            <td className="table-data">{props.networkPerformance}</td>
+            <td className="table-data">{props.vcpu}</td>
+            <td className="table-data">{props.storage}</td>
         </tr>
     );
 }
--- src/containers/price_list.js
+++ src/containers/price_list.js
@@ -15,7 +15,12 @@ export class PriceList extends Component {
                            leaseContractLength={priceData.leaseContractLength}
                            purchaseOption={priceData.purchaseOption}
                            offeringClass={priceData.offeringClass}
-                           pricePerUnit={priceData.pricePerUnit}/>);
+                           pricePerUnit={priceData.pricePerUnit}
+                           memory={priceData.memory}
+                           networkPerformance={priceData.networkPerformance}
+                           vcpu={priceData.vcpu}
+                           storage={priceData.storage}
+        />);
     }
 
     render() {
@@ -25,13 +30,17 @@ export class PriceList extends Component {
                 <table className="table table-hover table-striped">
                     <thead>
                     <tr>
-                        <th>Instance Name</th>
-                        <th>Region</th>
-                        <th>Term</th>
-                        <th>Lease Length</th>
-                        <th>Purchase Option</th>
-                        <th>Offering Class</th>
-                        <th>Price per hour</th>
+                        <th className="table-data">Instance</th>
+                        <th className="table-data">Region</th>
+                        <th className="table-data">Term</th>
+                        <th className="table-data">Lease</th>
+                        <th className="table-data">Purchase Option</th>
+                        <th className="table-data">Offering Class</th>
+                        <th className="table-data">Price</th>
+                        <th className="table-data">Memory</th>
+                        <th className="table-data">Network</th>
+                        <th className="table-data">vcpu</th>
+                        <th className="table-data">Storage</th>
                     </tr>
                     </thead>
                     <tbody>
